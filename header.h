f2028e28b6685b608b7d1888f790929281e023c1
dileep <dileepreddygobburi@gmail.com>
2022-05-31 17:20:00 +0530

    ORS project main header file

---
+++ header.h
@@ -0,0 +1,26 @@
+
+/**************HEADER FILE*****************************/
+#include<stdio.h>
+#include<stdlib.h>
+
+/******************FUNCTION PROTOTYPES*****************/
+void main();
+
+void training();
+
+void voting();
+
+void training_registration();
+
+void voting_request();
+
+void voting_response_module();
+
+void feedback_module();
+
+
+
+
+
+
+
